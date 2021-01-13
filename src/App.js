160c77fdf49e6d626882764bcba35cb40e08277f
Eric Humphries <ezhumphries@gmail.com>
2021-01-13 12:19:09 -0800

    added linting job to circleci config as well as added eslint script and pkgs to project

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import logo from "./logo.svg";
-import "./App.css";
+/* eslint-disable react/react-in-jsx-scope */
+import logo from './logo.svg';
+import './App.css';
 
 const App = () => {
   return (
@@ -9,12 +10,7 @@ const App = () => {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
