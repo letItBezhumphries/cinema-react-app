c2c60b88f7fd395d9042829d18bcca5041bdd523
Eric Humphries <ezhumphries@gmail.com>
2021-01-15 12:29:50 -0800

    fixed typo error in movieReducer involving action payload param

--- src/redux/reducers/errorReducer.js
+++ src/redux/reducers/errorReducer.js
@@ -1,7 +1,7 @@
 const initialState = '';
 
 export default (state = initialState, action) => {
-  switch ((action.type, payload)) {
+  switch ((action.type, action.payload)) {
     default:
       return state;
   }
