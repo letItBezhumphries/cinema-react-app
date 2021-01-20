8dd6e0e2318ef00606a753ef474e374d0143f732
Eric Humphries <ezhumphries@gmail.com>
2021-01-20 15:48:33 -0800

    added fetchData method to Main component

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -1,10 +1,17 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './Main.scss';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { loadMoreMovies, setResponsePageNumber } from '../../redux/actions/movies';
 import MainContent from '../content/main-content/MainContent';
 import Spinner from '../spinner/Spinner';
 
-const Main = () => {
+const Main = (props) => {
+  const { loadMoreMovies, setResponsePageNumber, page, totalPages } = props;
   const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(page);
+  const mainRef = useRef();
+  const bottomLineRef = useRef();
 
   useEffect(() => {
     setLoading(true);
@@ -12,7 +19,51 @@ const Main = () => {
       setLoading(false);
     }, 3000);
   }, []);
-  return <div className="main">{loading ? <Spinner /> : <MainContent />}</div>;
+
+  useEffect(() => {
+    setResponsePageNumber(currentPage, totalPages);
+    loadMoreMovies('now_playing', currentPage);
+  }, [currentPage]);
+
+  const fetchData = () => {
+    if (page < totalPages) {
+      setCurrentPage((prev) => prev + 1);
+    }
+  };
+
+  const handleScroll = () => {
+    // to get the height from getBoundingClientRect
+    const containerHeight = mainRef.current.getBoundingClientRect().height;
+    // destructuring and renaming top to bottomLineTop
+    const { top: bottomLineTop } = bottomLineRef.current.getBoundingClientRect();
+    if (bottomLineTop <= containerHeight) {
+      fetchData();
+    }
+  };
+
+  return (
+    <div className="main" ref={mainRef} onScroll={() => handleScroll()}>
+      {loading ? <Spinner /> : <MainContent />}
+      <div ref={bottomLineRef}></div>
+    </div>
+  );
 };
 
-export default Main;
+Main.propTypes = {
+  list: PropTypes.array.isRequired,
+  page: PropTypes.number.isRequired,
+  totalPages: PropTypes.number.isRequired,
+  loadMoreMovies: PropTypes.func.isRequired,
+  setResponsePageNumber: PropTypes.func.isRequired
+};
+
+const mapStateToProps = (state) => ({
+  list: state.movies.list,
+  page: state.movies.page,
+  totalPages: state.movies.totalPages
+});
+
+export default connect(mapStateToProps, {
+  loadMoreMovies,
+  setResponsePageNumber
+})(Main);
