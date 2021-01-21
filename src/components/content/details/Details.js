eb03b60731f60f07ef3e4b86c92448f42645ce5a
Eric Humphries <ezhumphries@gmail.com>
2021-01-21 11:53:05 -0800

    Added the details and main component routes

---
+++ src/components/content/details/Details.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Details.scss';
+
+const Details = () => {
+  return (
+    <div>
+      <h1>Details Component</h1>
+    </div>
+  );
+};
+
+export default Details;
